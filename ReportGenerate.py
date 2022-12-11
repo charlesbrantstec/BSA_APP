19f061e333246965333f6250ee664095a0659577
charles <charlesbrantstec@gmail.com>
2022-12-10 18:38:22 -0600

    working on report doc generation

--- ReportGenerate.py
+++ ReportGenerate.py
@@ -3,10 +3,19 @@ from docx.shared import Inches
 from docx.shared import Pt
 
 document = Document()
+style = document.styles['Normal']
+font = style.font
+font.name = 'Times New Roman'
+font.size = Pt(12)
 
-run = document.add_paragraph('Hi').add_run()
-font = run.font
-font.name = 'Calibri'   
-font.size = Pt(26)
+p = document.add_paragraph()
+l1 = p.add_run('1.Name:____________________________________________________________')
+l2 = p.add_run('  Address:_________________________________________________________')	
+l2 = p.add_run('  EIN/ITIN/SS#:_____________________________')	
+l3 = p.add_run('  1099 Amount Paid: $______________ W2 Amount Paid: $______________')			
+
+
+
+
 
 document.save('test.docx')
--- TestDoc.py
+++ TestDoc.py
@@ -10,38 +10,38 @@ p = document.add_paragraph('A plain paragraph having some ')
 p.add_run('bold').bold = True
 p.add_run(' and some ')
 p.add_run('italic.').italic = True
-run = p.add_run()
+run = p.add_run('TEST')
 font = run.font
-font.size = Pt(26)
+font.size = Pt(24)
 
-document.add_heading('Heading, level 1', level=1)
-document.add_paragraph('Intense quote', style='Intense Quote')
+# document.add_heading('Heading, level 1', level=1)
+# document.add_paragraph('Intense quote', style='Intense Quote')
 
-document.add_paragraph(
-    'first item in unordered list', style='List Bullet'
-)
-document.add_paragraph(
-    'first item in ordered list', style='List Number'
-)
+# document.add_paragraph(
+#     'first item in unordered list', style='List Bullet'
+# )
+# document.add_paragraph(
+#     'first item in ordered list', style='List Number'
+# )
 
-# document.add_picture('monty-truth.png', width=Inches(1.25))
+# # document.add_picture('monty-truth.png', width=Inches(1.25))
 
-records = (
-    (3, '101', 'Spam'),
-    (7, '422', 'Eggs'),
-    (4, '631', 'Spam, spam, eggs, and spam')
-)
+# records = (
+#     (3, '101', 'Spam'),
+#     (7, '422', 'Eggs'),
+#     (4, '631', 'Spam, spam, eggs, and spam')
+# )
 
-table = document.add_table(rows=1, cols=3)
-hdr_cells = table.rows[0].cells
-hdr_cells[0].text = 'Qty'
-hdr_cells[1].text = 'Id'
-hdr_cells[2].text = 'Desc'
-for qty, id, desc in records:
-    row_cells = table.add_row().cells
-    row_cells[0].text = str(qty)
-    row_cells[1].text = id
-    row_cells[2].text = desc
+# table = document.add_table(rows=1, cols=3)
+# hdr_cells = table.rows[0].cells
+# hdr_cells[0].text = 'Qty'
+# hdr_cells[1].text = 'Id'
+# hdr_cells[2].text = 'Desc'
+# for qty, id, desc in records:
+#     row_cells = table.add_row().cells
+#     row_cells[0].text = str(qty)
+#     row_cells[1].text = id
+#     row_cells[2].text = desc
 
 document.add_page_break()
 
