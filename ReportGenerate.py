39fb7975e5d9bfb5c7cb12e0a69eaa89ddfe0cc8
Charles Brant-Stec <charlesbrantstec@gmail.com>
2022-12-10 23:02:27 -0600

    continued work on doc gen

--- ReportGenerate.py
+++ ReportGenerate.py
@@ -10,13 +10,15 @@ font.name = 'Times New Roman'
 font.size = Pt(12)
 
 paragraph_format = style.paragraph_format
-paragraph_format.line_spacing = Pt(24)
+# paragraph_format.line_spacing = Pt(24)
 
 p = document.add_paragraph()
-l1 = p.add_run('1.Name:____________________________________________________________')
-l2 = p.add_run('  Address:_________________________________________________________')	
-l2 = p.add_run('  EIN/ITIN/SS#:_____________________________')	
-l3 = p.add_run('  1099 Amount Paid: $______________ W2 Amount Paid: $______________')			
+# p.alignment = WD_ALIGN_PARAGRAPH.CENTER
+l1 = p.add_run('1.Name:____________________________________________________________   \n    \
+                  Address:_________________________________________________________')
+# l2 = p.add_run('  Address:_________________________________________________________')	
+# l2 = p.add_run('  EIN/ITIN/SS#:_____________________________')	
+# l3 = p.add_run('  1099 Amount Paid: $______________ W2 Amount Paid: $______________')			
 
 
 
