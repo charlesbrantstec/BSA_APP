1f0da6cfda0c3c31b5ea426caf87fe09b74edfea
charles <charlesbrantstec@gmail.com>
2023-11-26 00:55:23 -0600

    new design draft

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class HelloWorld(QMainWindow):
     def __init__(self):
         super().__init__()
         self.browser = QWebEngineView()
-        self.browser.load(QUrl.fromLocalFile("index.html")) # Replace with the correct path
+        self.browser.load(QUrl.fromLocalFile(r"C:\Users\12158\Desktop\BSA_APP\html\job-portal-website-template\index.html")) # Replace with the correct path
         self.setCentralWidget(self.browser)
 
 if __name__ == '__main__':
